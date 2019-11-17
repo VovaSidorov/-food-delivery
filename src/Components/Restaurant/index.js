5d4d33ed4c191c7a43aa222718ee640650f68a50
VovaSidorov <VovaCivic@gmail.com>
2019-11-17 22:08:21 +0200

    Add accordeon functionality

--- src/Components/Restaurant/index.js
+++ src/Components/Restaurant/index.js
@@ -2,15 +2,25 @@ import React, { Component } from "react";
 import RestaurantMenu from "../RestaurantMenu";
 
 class Restaurant extends Component {
+  state = {
+    isOpen: false
+  };
   render() {
-    const { image, name, menu } = this.props;
+    const { image, name, menu, isMenuOpen } = this.props;
+
     return (
       <div>
         <img src={image} width={64} height={64} />
         <h3>{name}</h3>
-        <RestaurantMenu menu={menu} />
+        <button onClick={() => this.handleToogleOpenClick()}>
+          {isMenuOpen ? "Close Menu" : "Open Menu"}
+        </button>
+        {isMenuOpen ? <RestaurantMenu menu={menu} /> : null}
       </div>
     );
   }
+  handleToogleOpenClick = () => {
+    this.props.toogleOpenMenu();
+  };
 }
 export default Restaurant;
--- src/Components/restaurantList/index.js
+++ src/Components/restaurantList/index.js
@@ -2,15 +2,29 @@ import React, { Component } from "react";
 import Restaurant from "../Restaurant";
 
 class RestaurantList extends Component {
+  state = {
+    openRestaurentMenuId: null
+  };
   render() {
     const { restaurants } = this.props;
+    const { openRestaurentMenuId } = this.state;
     return (
       <div>
         {restaurants.map(restaurants => (
-          <Restaurant key={restaurants.id} {...restaurants} />
+          <Restaurant
+            key={restaurants.id}
+            {...restaurants}
+            isMenuOpen={openRestaurentMenuId === restaurants.id}
+            toogleOpenMenu={() => this.toogleOpenRestaurantMenu(restaurants.id)}
+          />
         ))}
       </div>
     );
   }
+  toogleOpenRestaurantMenu = id => {
+    this.setState({
+      openRestaurentMenuId: id
+    });
+  };
 }
 export default RestaurantList;
